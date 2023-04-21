b0d5f8c571db408ab60a51a49509d40922aa42e5
AIHardCore <aihardcore@163.com>
2023-04-21 18:58:36 +0800

    修复登录页验证的bug

--- src/views/login.vue
+++ src/views/login.vue
@@ -13,8 +13,8 @@
     <div class="login">
       <div class="logo">
         <img
-          :src="require('../assets/logo1.png')"
-          style="width:60px;height:60px">
+            :src="require('../assets/logo1.png')"
+            style="width:60px;height:60px">
       </div>
       <h2 v-if="showPage">欢迎登录
       </h2>
@@ -22,72 +22,92 @@
         用户注册
       </h2>
       <el-card class="forms"
-        v-if="showPage"
-        shadow="never">
+               v-if="showPage"
+               shadow="never">
         <el-form :model="form"
-          :rules="rules"
-          ref="ruleForm"
-          label-position="top"
-          label-width="0px"
-          class="demo-ruleForm">
+                 :rules="rules"
+                 ref="ruleForm"
+                 label-position="top"
+                 label-width="0px"
+                 class="demo-ruleForm">
           <el-form-item
-            prop="mobile">
+              prop="mobile">
             <el-input
-              prefix-icon="el-icon-mobile-phone"
-              placeholder="请输入手机号"
-              v-model="form.mobile"></el-input>
+                prefix-icon="el-icon-mobile-phone"
+                placeholder="请输入手机号"
+                v-model="form.mobile"></el-input>
           </el-form-item>
           <el-form-item
-            prop="password">
+              prop="password">
             <el-input
-              :type="type"
-              prefix-icon="el-icon-s-goods"
-              placeholder="请输入密码"
-              v-model="form.password">
+                :type="type"
+                prefix-icon="el-icon-s-goods"
+                placeholder="请输入密码"
+                v-model="form.password">
               <i slot="suffix"
-                @click="type == 'password'? type ='text':type ='password'"
-                class="el-input__icon el-icon-view"></i>
+                 @click="type == 'password'? type ='text':type ='password'"
+                 class="el-input__icon el-icon-view"></i>
             </el-input>
           </el-form-item>
           <el-form-item
-            style="margin-bottom: 0;">
-            <el-button
-              type="primary"
-              @click="login"
-              style="width:100%">登录</el-button>
+              prop="name1" v-show="false" >
+            <el-input
+                prefix-icon="el-icon-user-solid"
+                placeholder="请输入姓名"
+                v-model="regform.name1"></el-input>
           </el-form-item>
           <el-form-item
-            style="margin-bottom: 0;">
+              prop="msgCode1" v-show="false">
+            <el-input
+                prefix-icon="el-icon-folder-checked"
+                placeholder="请输入验证码"
+                v-model="regform.msgCode1">
+              <el-button
+                  slot="append"
+                  @click="getCode"
+                  :disabled="disabled"
+                  type="primary">{{ codeText }}</el-button>
+            </el-input>
+          </el-form-item>
+          <el-form-item
+              style="margin-bottom: 0;">
+            <el-button
+                type="primary"
+                @click="login"
+                style="width:100%">登录</el-button>
+          </el-form-item>
+          <el-form-item
+              style="margin-bottom: 0;">
           </el-form-item>
         </el-form>
       </el-card>
       <el-card class="forms"
-        shadow="never"
-        v-if="!showPage">
+               shadow="never"
+               v-if="!showPage">
         <el-form
-          :model="regform"
-          :rules="regrules"
-          ref="regform"
-          label-width="0px"
-          class="demo-ruleForm">
+            :model="regform"
+            :rules="regrules"
+            ref="regform"
+            label-width="0px"
+            class="demo-ruleForm">
           <el-form-item
-            prop="mobile">
+              prop="mobile">
             <el-input
-              type ='text'
-              prefix-icon="el-icon-mobile-phone"
-              placeholder="请输入手机号"
-              v-model="regform.mobile"></el-input>
+                type ='text'
+                prefix-icon="el-icon-mobile-phone"
+                placeholder="请输入手机号"
+                v-model="regform.mobile"></el-input>
           </el-form-item>
           <el-form-item
-            prop="password">
+              prop="password">
             <el-input
-              :type="type"
-              prefix-icon="el-icon-s-goods"
-              placeholder="请输入密码"
-              v-model="regform.password">
+                :type="type"
+                prefix-icon="el-icon-s-goods"
+                placeholder="请输入密码"
+                v-model="regform.password">
               <i slot="suffix"
-                @click="type == 'password'? type ='text':type ='password'"
-                class="el-input__icon el-icon-view"></i>
+                 @click="type == 'password'? type ='text':type ='password'"
+                 class="el-input__icon el-icon-view"></i>
             </el-input>
           </el-form-item>
           <el-form-item
@@ -98,36 +118,46 @@
                 v-model="regform.name"></el-input>
           </el-form-item>
           <el-form-item
-            prop="msgCode">
+              prop="msgCode">
             <el-input
-              prefix-icon="el-icon-folder-checked"
-              placeholder="请输入验证码"
-              v-model="regform.msgCode">
+                prefix-icon="el-icon-folder-checked"
+                placeholder="请输入验证码"
+                v-model="regform.msgCode">
               <el-button
-                slot="append"
-                @click="getCode"
-                :disabled="disabled"
-                type="primary">{{ codeText }}</el-button>
+                  slot="append"
+                  @click="getCode"
+                  :disabled="disabled"
+                  type="primary">{{ codeText }}</el-button>
             </el-input>
           </el-form-item>
-        <el-form-item
-          style="margin-bottom: 0;">
-          <el-button
-            type="primary"
-            @click="reglogin"
-            style="width:100%">
-            注册</el-button>
-        </el-form-item>
+          <el-form-item
+              style="margin-bottom: 0;">
+            <el-button
+                type="primary"
+                @click="reglogin"
+                style="width:100%">
+              注册</el-button>
+          </el-form-item>
         </el-form>
       </el-card>
       <el-button
-        v-if="showPage"
-        @click="reg"
-        style="width:350px;margin-top:30px">还没有账号？立即注册</el-button>
+          v-if="showPage"
+          @click="reg"
+          style="width:350px;margin-top:30px">还没有账号？立即注册</el-button>
       <el-button
-        v-if="!showPage"
-        @click="reg"
-        style="width:350px;margin-top:30px">已有账号，立即登录</el-button>
+          v-if="!showPage"
+          @click="reg"
+          style="width:350px;margin-top:30px">已有账号，立即登录</el-button>
+    </div>
+    <div id="app">
+      <router-view />
+      <div style="position:fixed;text-align:center;bottom:0;margin:0 auto;width:100%;color: #5c6b77">
+        <a
+            target="_blank"
+            style="color: #5c6b77"
+            href="https://beian.miit.gov.cn/">京ICP备00000001</a>
+        &nbsp; @2023-2024 HardCore
+      </div>
     </div>
   </div>
 </template>
@@ -145,9 +175,11 @@ export default {
       time: '',
       regform: {
         name: '',
+        name1: '',
         mobile: '',
         password: '',
-        msgCode: ''
+        msgCode: '',
+        msgCode1: '',
       },
       disabled: false,
       timer: null,
@@ -155,12 +187,15 @@ export default {
       regrules: {
         name: [
           { required: true, message: '请输入姓名', trigger: 'blur' },
-          { max: 8, min: 2, message: '不能超过8个字符', trigger: 'blur' }
+          { max: 8, min: 2, message: '姓名长度在2-8个字符之间', trigger: 'blur' }
         ],
-        msgCode: [{ required: true, message: '请输入验证码', trigger: 'blur' }],
+        msgCode: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { max: 6, min: 6, message: '请输入6位验证码', trigger: 'blur' }
+        ],
         mobile: [
           { required: true, message: '请输入手机号', trigger: 'blur' },
-          { max: 11, min: 11, message: '手机号格式错误', trigger: 'change' }
+          { max: 11, min: 11, message: '手机号格式错误', trigger: 'blur' }
         ],
         password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
       },
@@ -170,7 +205,7 @@ export default {
           { required: true, message: '请输入手机号', trigger: 'change' },
           { max: 11, min: 11, message: '手机号格式错误', trigger: 'change' }
         ],
-        password: [{ required: true, message: '请输入密码', trigger: 'change' }]
+        password: [{ required: true, message: '请输入密码', trigger: 'change' }],
       }
     }
   },
@@ -243,7 +278,7 @@ export default {
     },
     _isMobile() {
       let flag = navigator.userAgent.match(
-        /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+          /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
       )
       return flag
     }
