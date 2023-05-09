7cf35d1961d6970f58984e411c60a6301d5a0434
AIHardCore <aihardcore@163.com>
2023-05-09 09:17:30 +0800

    打包压缩

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,23 @@
  */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 3000
-  }
+    productionSourceMap: false,
+    transpileDependencies: true,
+    devServer: {
+        port: 3000
+    },
+    publicPath: './',
+    chainWebpack: config => {
+        const CompressionWebpackPlugin = require('compression-webpack-plugin')
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin('CompressionPlugin').use(
+                new CompressionWebpackPlugin({
+                    test: /\.(js|css)$/,
+                    threshold: 10240, // 超过10kb的文件就压缩
+                    deleteOriginalAssets:false, // 不删除源文件
+                    minRatio: 0.8
+                })
+            )
+        }
+    }
 })
