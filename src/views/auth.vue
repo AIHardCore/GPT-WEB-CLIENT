fe57caba524a55e53514874ad8c35dbd13aad3b0
AIHardCore <aihardcore@163.com>
2023-05-09 09:16:03 +0800

    微信授权

--- src/views/auth.vue
+++ src/views/auth.vue
@@ -44,6 +44,12 @@ export default {
     }
   },
   methods: {
+    _isMobile() {
+      let flag = navigator.userAgent.match(
+          /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+      )
+      return flag
+    },
     login() {
       this.$https('WXLOGIN',{
         code: this.code
